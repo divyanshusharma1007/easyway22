9ee5f5539fdd2c79175f66173f489f71c63bec26
Divyanshu Sharma <skprogrammer1007@gmail.com>
2022-08-14 14:43:14 +0530

    push 5

--- pages/login.js
+++ pages/login.js
@@ -2,10 +2,9 @@ import React from 'react'
 import Image from 'next/image'
 export default function login() {
     return (
-        <div className='bg-gray-200 py-10'>
-
-            <section className="h-full gradient-form md:h-screen ">
-                <div className="container py-12 px-6 h-full">
+        <div className='bg-gray-200 py-10 relative '>
+            <section className="h-full gradient-form md:h-auto ">
+                <div className="container py-12 px-6 ">
                     <div className="flex justify-center items-center flex-wrap h-full g-6 text-gray-800">
                         <div className="xl:w-10/12">
                             <div className="block bg-white shadow-lg rounded-lg">
