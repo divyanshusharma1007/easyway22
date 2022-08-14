b0dba18c12b7f9a9ba9fa61c5ba7e65d5c5fe04d
Divyanshu Sharma <skprogrammer1007@gmail.com>
2022-08-14 16:50:26 +0530

    pushed

--- pages/cart.js
+++ pages/cart.js
@@ -11,7 +11,7 @@ export default function Cart() {
 
 
             </div>
-            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded fixed  right-0">
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded fixed  right-0">
                 Check Out
             </button>
         </>
