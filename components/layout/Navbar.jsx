6c8f845c52ecba6b83848f69b0274337f02e6809
Divyanshu Sharma <skprogrammer1007@gmail.com>
2022-08-14 14:22:58 +0530

    nav routes added

--- components/layout/Navbar.jsx
+++ components/layout/Navbar.jsx
@@ -12,10 +12,10 @@ export default function Navbar() {
 
             <Link href="/">Home </Link></a>
           <a className="mr-5 hover:text-white font-extrabold"><Link href="/order">Order</Link></a>
-          <a className="mr-5 hover:text-white font-extrabold"><Link href="/">About</Link></a>
+          <a className="mr-5 hover:text-white font-extrabold"><Link href="/about">About</Link></a>
           <a className="mr-5 hover:text-white font-extrabold flex cursor-pointer">
 
-            <Link href="/">
+            <Link href="/cart">
               <>
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
                   <path strokeLinecap="round" strokeLinejoin="round" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
@@ -24,7 +24,7 @@ export default function Navbar() {
               </>
             </Link></a>
           <button className='mr-5 hover:text-white font-extrabold flex cursor-pointer'>
-            <Link href="login">
+            <Link href="/login">
               Login
             </Link>
           </button>
