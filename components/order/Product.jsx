6008a4a5a30a722e924d2ce249cc9b1e6e5a5b03
Divyanshu Sharma <skprogrammer1007@gmail.com>
2022-08-24 18:44:12 +0530

    up data

--- components/order/Product.jsx
+++ components/order/Product.jsx
@@ -6,7 +6,7 @@ export default function Product({prduc}) {
         <div>
             <h1 className='hidden md:block text-center text-slate-500 text-2xl font-medium font-serif m-5'>Products </h1>
             <div className="grid sm:grid-cols-3 lg:grid-cols-4 mt-3 mx-3">
-                {prduc?.map(e=><ProductCard product={e}/>)}
+                {prduc?.map(e=><ProductCard product={e} key={e._id}/>)}
             </div>
 
         </div>
