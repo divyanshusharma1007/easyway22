22a7485763c25ba46ebef676d5e911c33f77cc88
Divyanshu Sharma <skprogrammer1007@gmail.com>
2022-08-26 15:32:00 +0530

    dashboard backend added

--- database/connections.js
+++ database/connections.js
@@ -3,10 +3,12 @@ const mongoose = require('mongoose');
 import MONGO_URI from './dbsettings'
 
 const connectToMongo = (handler) => async (req, res) => {
+    console.log("genrating connection")
     if (mongoose.connections[0].readyState) {
         return handler(req, res);
     }
     const data=await mongoose.connect(MONGO_URI)
+    console.log(data)
     return handler(req, res)
 }
 module.exports = connectToMongo;
---
+++ pages/api/admin/index.js
@@ -0,0 +1,24 @@
+import { model } from "mongoose";
+import connectToMongo from "../../../database/connections"
+import models from '../../../database/models'
+import fetchadmin from '../../../helpers/fetchadmin'
+const handler = async (req, res) => {
+    console.log("calling fetch admin")
+    if (fetchadmin(req, res)) {
+        const { Product, User, Order } = models();
+        try {
+            console.log("running")
+            const order = await Order.find();
+            const noProduct = await Product.find().count();
+            const noUsers = await User.find().count();
+            console.log("databse connected")
+            res.status(200).json({numberOfProducts:noProduct,numberOfOrder:await await order.count(),noOfUser:noUsers,order:order})
+        } catch (e) {
+            console.log(e)
+            res.status(400).json({ error: "some internal server error occured" })
+        }
+    }
+
+
+}
+export default connectToMongo(handler);
