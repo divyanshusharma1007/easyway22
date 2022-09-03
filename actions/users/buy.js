c20a3988a6b65506e69222dba5567b8104ad323e
Divyanshu Sharma <skprogrammer1007@gmail.com>
2022-09-03 15:34:19 +0530

    set data

--- actions/users/buy.js
+++ actions/users/buy.js
@@ -2,20 +2,24 @@ import axios from "axios";
 import Cookies from 'js-cookie';
 import serverurl from "../../serverurl";
 export default async (data) => {
+    try {
 
-    const options = {
-        method: 'POST',
-        url: `${serverurl}/user/buy`,
-        headers: {
-            'auth-token': await JSON.parse(Cookies.get('auth'))
-        },
-        data: {products:data}
-    };
-    axios.request(options).then(function (response) {
-        alert("your order is successfully placed ")
-        console.log(response.data);
-    }).catch(function (error) {
-        alert("your order is not placed , some error occured")
-        console.error(error);
-    });
+        const options = {
+            method: 'POST',
+            url: `${serverurl}/user/buy`,
+            headers: {
+                'auth-token': await JSON.parse(Cookies.get('auth'))
+            },
+            data: { products: data }
+        };
+        axios.request(options).then(function (response) {
+            alert("your order is successfully placed ")
+            console.log(response.data);
+        }).catch(function (error) {
+            alert("your order is not placed , some error occured")
+            console.error(error);
+        });
+    }catch (e){
+        alert("there is a errror ")
+    }
 }
